ad80182c1c042acdcd57203b8290ce3ff805dc4d
tocinochan033 <vazquezlmartin33@gmail.com>
2026-07-23 12:40:10 -0700

    TODO_HELP add

--- src/todo.rs
+++ src/todo.rs
@@ -1,3 +1,12 @@
+const TODO_HELP: &str =
+    "Basic todo app\
+    A quick CLI todo app—and I'm not sure what else.\
+    Available commands:\
+        add: Add a new task.\
+        edit: Edit an existing task\
+        list: View the full list of available tasks\
+        delete: Delete an existing task\
+        change status: Change the status of an existing task";
 
 enum Status{
     Pending,
