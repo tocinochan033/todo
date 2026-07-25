4b33169b768baa675a54231a9719016ce6dd2e0b
tocinochan033 <vazquezlmartin33@gmail.com>
2026-07-25 16:36:54 -0700

    function clear_screen

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,32 @@ mod menu;
 fn main() {
     let mut a = todo::Todo::new();
 
-    println!("Todo list in rust :v");
-    print!("entry: ");
+    loop {
+        println!("Todo list in rust :v");
+        print!("entry: ");
 
-    let mut entry = String::new();
-    std::io::stdin()
-        .read_line(&mut entry).
-        expect("Error stdin");
+        let mut entry = String::new();
+        std::io::stdin()
+            .read_line(&mut entry).
+            expect("Error stdin");
 
-    match entry.as_str() {
-        "add" => menu::add_task(&mut a),
-        "list" => todo::Todo::list(&a),
-        "delete" => {
-            let id: u8 = menu::delete_task(&mut a);
-            todo::Todo::delete_task(&mut a, id);
-        },
-        "edit" => menu::edit_task(&mut a),
-        "changes status" => menu::changes_status_to_do(&mut a),
-        _ => println!("SELECCIONE UNA OPCION VALIDA.\n {}", todo::TODO_HELP),
+        match entry.as_str() {
+            "add" => menu::add_task(&mut a),
+            "list" => todo::Todo::list(&a),
+            "delete" => {
+                let id: u8 = menu::delete_task(&mut a);
+                todo::Todo::delete_task(&mut a, id);
+            },
+            "edit" => menu::edit_task(&mut a),
+            "changes status" => menu::changes_status_to_do(&mut a),
+            _ => { println!("SELECCIONE UNA OPCION VALIDA.\n {}", todo::TODO_HELP); clear_screen() },
+        }
     }
+
+}
+
+fn clear_screen() {
+    // \x1B[2J borra toda la pantalla
+    // \x1B[0;0f mueve el cursor a la esquina superior izquierda
+    print!("\x1B[2J\x1B[0;0f");
 }
